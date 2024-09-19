a80fc0289b3a97133e15a4160d08d82efda5282d
Luke Farrell <lukefarrell2012@gmail.com>
2024-09-19 19:50:03 +0100

    Init on day 1

--- src/question.js
+++ src/question.js
@@ -4,4 +4,21 @@ class Question {
     // 1. constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
+    constructor (text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+    shuffleChoices () {
+        const newChoices = [];
+        let randomNumber = 0;
+        while (this.choices.length > 0){
+            randomNumber = Math.floor(Math.random(this.choices.length));
+            newChoices.push(this.choices[randomNumber]);
+            this.choices.splice(randomNumber, 1);
+        }
+        this.choices = newChoices;
+    }
 }
