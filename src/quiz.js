e14b0f140a8799786ef95fb0f3bb22b6a9c110b6
nehais <nehasinha82@gmail.com>
2024-09-19 21:26:52 +0200

    Quiz class done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,40 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions      = questions;
+        this.timeLimit      = timeLimit;
+        this.timeRemaining  = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex ++;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+        const randomisedArray = this.questions.sort(() => 0.5 - Math.random());
+        this.questions = randomisedArray;
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        let question = this.getQuestion();
+        if (answer ===  question.answer){
+            this.correctAnswers ++;
+        }
+    }
 
-    // 6. hasEnded()
+    hasEnded(){
+        if (this.currentQuestionIndex === this.questions.length){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
